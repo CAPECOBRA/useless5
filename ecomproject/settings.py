217f9f55b3a25d8cbbed3eee5b5d217e324f9e29
CAPECOBRA <786amitsingh86@gmail.com>
2021-09-06 23:47:08 +0530

    commit

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
