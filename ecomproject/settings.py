b2626cbbf317478c5ab5e54752ec22c66f42b950
CAPECOBRA <786amitsingh86@gmail.com>
2021-09-06 23:25:09 +0530

    commit

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1x+(0yub_lq_a24fcwgt=2e)n0vk))pht0^kh13-2!0e&q*e_r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'dailycart143.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','dailycart143.herokuapp.com']
 
 
 # Application definition
